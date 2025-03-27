da909a3a84b78dbb0cf8959faf5e400a45da4ff7
zouxu <izouxv@gmail.com>
2025-03-27 15:17:17 +0800

    remove stick

--- internal/sqlx/schema.sql
+++ internal/sqlx/schema.sql
@@ -9,218 +9,266 @@ pragma user_version = 1;
 -- 3 - set
 -- 4 - hash
 -- 5 - zset (sorted set)
-create table if not exists
-rkey (
-    id       integer primary key,
-    key      text not null,
-    type     integer not null,
-    version  integer not null,
-    etime    integer,
-    mtime    integer not null,
-    len      integer
-) strict;
+CREATE TABLE if NOT EXISTS rkey (
+  id INTEGER PRIMARY KEY,
+  KEY text NOT NULL,
+  TYPE INTEGER NOT NULL,
+  VERSION INTEGER NOT NULL,
+  etime INTEGER,
+  mtime INTEGER NOT NULL,
+  len INTEGER
+);
 
-create unique index if not exists
-rkey_key_idx on rkey (key);
+CREATE UNIQUE index if NOT EXISTS rkey_key_idx ON rkey (KEY);
 
-create index if not exists
-rkey_etime_idx on rkey (etime)
-where etime is not null;
+CREATE index if NOT EXISTS rkey_etime_idx ON rkey (etime)
+WHERE
+  etime IS NOT NULL;
 
-create view if not exists
-vkey as
-select
-    id as kid, key, type, len,
-    datetime(etime/1000, 'unixepoch') as etime,
-    datetime(mtime/1000, 'unixepoch') as mtime
-from rkey
-where rkey.etime is null or rkey.etime > unixepoch('subsec');
+CREATE VIEW if NOT EXISTS vkey AS
+SELECT
+  id AS kid,
+  KEY,
+  TYPE,
+  len,
+  datetime(etime / 1000, 'unixepoch') AS etime,
+  datetime(mtime / 1000, 'unixepoch') AS mtime
+FROM
+  rkey
+WHERE
+  rkey.etime IS NULL
+  OR rkey.etime > unixepoch('subsec');
 
 -- ┌───────────────┐
 -- │ Strings       │
 -- └───────────────┘
-create table if not exists
-rstring (
-    kid    integer not null,
-    value  blob not null,
+CREATE TABLE if NOT EXISTS rstring (
+  kid INTEGER NOT NULL,
+  VALUE BLOB NOT NULL,
+  FOREIGN KEY (kid) REFERENCES rkey (id) ON
+  DELETE
+    CASCADE
+);
 
-    foreign key (kid) references rkey (id)
-    on delete cascade
-) strict;
+CREATE UNIQUE index if NOT EXISTS rstring_pk_idx ON rstring (kid);
 
-create unique index if not exists
-rstring_pk_idx on rstring (kid);
-
-create view if not exists
-vstring as
-select
-    rkey.id as kid, rkey.key, rstring.value,
-    datetime(etime/1000, 'unixepoch') as etime,
-    datetime(mtime/1000, 'unixepoch') as mtime
-from rstring join rkey on rstring.kid = rkey.id and rkey.type = 1
-where rkey.etime is null or rkey.etime > unixepoch('subsec');
+CREATE VIEW if NOT EXISTS vstring AS
+SELECT
+  rkey.id AS kid,
+  rkey.key,
+  rstring.value,
+  datetime(etime / 1000, 'unixepoch') AS etime,
+  datetime(mtime / 1000, 'unixepoch') AS mtime
+FROM
+  rstring
+  JOIN rkey ON rstring.kid = rkey.id
+  AND rkey.type = 1
+WHERE
+  rkey.etime IS NULL
+  OR rkey.etime > unixepoch('subsec');
 
 -- ┌───────────────┐
 -- │ Lists         │
 -- └───────────────┘
-create table if not exists
-rlist (
-    kid    integer not null,
-    pos    real not null,
-    elem   blob not null,
+CREATE TABLE if NOT EXISTS rlist (
+  kid INTEGER NOT NULL,
+  pos REAL NOT NULL,
+  elem BLOB NOT NULL,
+  FOREIGN KEY (kid) REFERENCES rkey (id) ON
+  DELETE
+    CASCADE
+);
 
-    foreign key (kid) references rkey (id)
-    on delete cascade
-) strict;
+CREATE UNIQUE index if NOT EXISTS rlist_pk_idx ON rlist (kid, pos);
 
-create unique index if not exists
-rlist_pk_idx on rlist (kid, pos);
+CREATE TRIGGER if NOT EXISTS rlist_on_update BEFORE
+UPDATE
+  ON rlist FOR EACH ROW BEGIN
+UPDATE
+  rkey
+SET
+  VERSION = VERSION + 1,
+  mtime = unixepoch('subsec') * 1000
+WHERE
+  id = OLD.kid;
 
-create trigger if not exists
-rlist_on_update
-before update on rlist
-for each row
-begin
-    update rkey set
-        version = version + 1,
-        mtime = unixepoch('subsec') * 1000
-    where id = old.kid;
-end;
+END;
 
-create trigger if not exists
-rlist_on_delete
-before delete on rlist
-for each row
-begin
-    update rkey set
-        version = version + 1,
-        mtime = unixepoch('subsec') * 1000,
-        len = len - 1
-    where id = old.kid;
-end;
+CREATE TRIGGER if NOT EXISTS rlist_on_delete BEFORE
+DELETE
+  ON rlist FOR EACH ROW BEGIN
+UPDATE
+  rkey
+SET
+  VERSION = VERSION + 1,
+  mtime = unixepoch('subsec') * 1000,
+  len = len - 1
+WHERE
+  id = OLD.kid;
 
-create view if not exists
-vlist as
-select
-    rkey.id as kid, rkey.key,
-    row_number() over w as idx, rlist.elem,
-    datetime(etime/1000, 'unixepoch') as etime,
-    datetime(mtime/1000, 'unixepoch') as mtime
-from rlist join rkey on rlist.kid = rkey.id and rkey.type = 2
-where rkey.etime is null or rkey.etime > unixepoch('subsec')
-window w as (partition by kid order by pos);
+END;
+
+CREATE VIEW if NOT EXISTS vlist AS
+SELECT
+  rkey.id AS kid,
+  rkey.key,
+  ROW_NUMBER() OVER w AS idx,
+  rlist.elem,
+  datetime(etime / 1000, 'unixepoch') AS etime,
+  datetime(mtime / 1000, 'unixepoch') AS mtime
+FROM
+  rlist
+  JOIN rkey ON rlist.kid = rkey.id
+  AND rkey.type = 2
+WHERE
+  rkey.etime IS NULL
+  OR rkey.etime > unixepoch('subsec') WINDOW w AS (
+    PARTITION BY kid
+    ORDER
+      BY pos
+  );
 
 -- ┌───────────────┐
 -- │ Sets          │
 -- └───────────────┘
-create table if not exists
-rset (
-    kid    integer not null,
-    elem   blob not null,
+CREATE TABLE if NOT EXISTS rset (
+  kid INTEGER NOT NULL,
+  elem BLOB NOT NULL,
+  FOREIGN KEY (kid) REFERENCES rkey (id) ON
+  DELETE
+    CASCADE
+);
 
-    foreign key (kid) references rkey (id)
-    on delete cascade
-) strict;
+CREATE UNIQUE index if NOT EXISTS rset_pk_idx ON rset (kid, elem);
 
-create unique index if not exists
-rset_pk_idx on rset (kid, elem);
+CREATE TRIGGER if NOT EXISTS rset_on_insert
+AFTER
+INSERT
+  ON rset FOR EACH ROW BEGIN
+UPDATE
+  rkey
+SET
+  len = len + 1
+WHERE
+  id = NEW.kid;
 
-create trigger if not exists
-rset_on_insert
-after insert on rset
-for each row
-begin
-    update rkey
-    set len = len + 1
-    where id = new.kid;
-end;
+END;
 
-create view if not exists
-vset as
-select
-    rkey.id as kid, rkey.key, rset.elem,
-    datetime(etime/1000, 'unixepoch') as etime,
-    datetime(mtime/1000, 'unixepoch') as mtime
-from rset join rkey on rset.kid = rkey.id and rkey.type = 3
-where rkey.etime is null or rkey.etime > unixepoch('subsec');
+CREATE VIEW if NOT EXISTS vset AS
+SELECT
+  rkey.id AS kid,
+  rkey.key,
+  rset.elem,
+  datetime(etime / 1000, 'unixepoch') AS etime,
+  datetime(mtime / 1000, 'unixepoch') AS mtime
+FROM
+  rset
+  JOIN rkey ON rset.kid = rkey.id
+  AND rkey.type = 3
+WHERE
+  rkey.etime IS NULL
+  OR rkey.etime > unixepoch('subsec');
 
 -- ┌───────────────┐
 -- │ Hashes        │
 -- └───────────────┘
-create table if not exists
-rhash (
-    kid   integer not null,
-    field text not null,
-    value blob not null,
+CREATE TABLE if NOT EXISTS rhash (
+  kid INTEGER NOT NULL,
+  field text NOT NULL,
+  VALUE BLOB NOT NULL,
+  FOREIGN KEY (kid) REFERENCES rkey (id) ON
+  DELETE
+    CASCADE
+);
 
-    foreign key (kid) references rkey (id)
-    on delete cascade
-) strict;
+CREATE UNIQUE index if NOT EXISTS rhash_pk_idx ON rhash (kid, field);
 
-create unique index if not exists
-rhash_pk_idx on rhash (kid, field);
+CREATE TRIGGER if NOT EXISTS rhash_on_insert BEFORE
+INSERT
+  ON rhash FOR EACH ROW
+  WHEN (
+    SELECT
+      COUNT(*)
+    FROM
+      rhash
+    WHERE
+      kid = NEW.kid
+      AND field = NEW.field
+  ) = 0 BEGIN
+UPDATE
+  rkey
+SET
+  len = len + 1
+WHERE
+  id = NEW.kid;
 
-create trigger if not exists
-rhash_on_insert
-before insert on rhash
-for each row
-when (
-        select count(*) from rhash
-        where kid = new.kid and field = new.field
-    ) = 0
-begin
-    update rkey
-    set len = len + 1
-    where id = new.kid;
-end;
+END;
 
-create view if not exists
-vhash as
-select
-    rkey.id as kid, rkey.key, rhash.field, rhash.value,
-    datetime(etime/1000, 'unixepoch') as etime,
-    datetime(mtime/1000, 'unixepoch') as mtime
-from rhash join rkey on rhash.kid = rkey.id and rkey.type = 4
-where rkey.etime is null or rkey.etime > unixepoch('subsec');
+CREATE VIEW if NOT EXISTS vhash AS
+SELECT
+  rkey.id AS kid,
+  rkey.key,
+  rhash.field,
+  rhash.value,
+  datetime(etime / 1000, 'unixepoch') AS etime,
+  datetime(mtime / 1000, 'unixepoch') AS mtime
+FROM
+  rhash
+  JOIN rkey ON rhash.kid = rkey.id
+  AND rkey.type = 4
+WHERE
+  rkey.etime IS NULL
+  OR rkey.etime > unixepoch('subsec');
 
 -- ┌───────────────┐
 -- │ Sorted sets   │
 -- └───────────────┘
-create table if not exists
-rzset (
-    kid    integer not null,
-    elem   blob not null,
-    score  real not null,
+CREATE TABLE if NOT EXISTS rzset (
+  kid INTEGER NOT NULL,
+  elem BLOB NOT NULL,
+  score REAL NOT NULL,
+  FOREIGN KEY (kid) REFERENCES rkey (id) ON
+  DELETE
+    CASCADE
+);
 
-    foreign key (kid) references rkey (id)
-    on delete cascade
-) strict;
+CREATE UNIQUE index if NOT EXISTS rzset_pk_idx ON rzset (kid, elem);
 
-create unique index if not exists
-rzset_pk_idx on rzset (kid, elem);
+CREATE index if NOT EXISTS rzset_score_idx ON rzset (kid, score, elem);
 
-create index if not exists
-rzset_score_idx on rzset (kid, score, elem);
+CREATE TRIGGER if NOT EXISTS rzset_on_insert BEFORE
+INSERT
+  ON rzset FOR EACH ROW
+  WHEN (
+    SELECT
+      COUNT(*)
+    FROM
+      rzset
+    WHERE
+      kid = NEW.kid
+      AND elem = NEW.elem
+  ) = 0 BEGIN
+UPDATE
+  rkey
+SET
+  len = len + 1
+WHERE
+  id = NEW.kid;
 
-create trigger if not exists
-rzset_on_insert
-before insert on rzset
-for each row
-when (
-        select count(*) from rzset
-        where kid = new.kid and elem = new.elem
-    ) = 0
-begin
-    update rkey
-    set len = len + 1
-    where id = new.kid;
-end;
+END;
 
-create view if not exists
-vzset as
-select
-    rkey.id as kid, rkey.key, rzset.elem, rzset.score,
-    datetime(etime/1000, 'unixepoch') as etime,
-    datetime(mtime/1000, 'unixepoch') as mtime
-from rzset join rkey on rzset.kid = rkey.id and rkey.type = 5
-where rkey.etime is null or rkey.etime > unixepoch('subsec');
+CREATE VIEW if NOT EXISTS vzset AS
+SELECT
+  rkey.id AS kid,
+  rkey.key,
+  rzset.elem,
+  rzset.score,
+  datetime(etime / 1000, 'unixepoch') AS etime,
+  datetime(mtime / 1000, 'unixepoch') AS mtime
+FROM
+  rzset
+  JOIN rkey ON rzset.kid = rkey.id
+  AND rkey.type = 5
+WHERE
+  rkey.etime IS NULL
+  OR rkey.etime > unixepoch('subsec');
